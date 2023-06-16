0245c511e22a151f98c0895ffc97baa765b47a1c
Hugo Gzz Saenz <hgogzzs@gmail.com>
2023-06-15 20:58:30 -0600

    primercommit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export const NavBar = () => (
+    <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+    </Navbar>
+)
